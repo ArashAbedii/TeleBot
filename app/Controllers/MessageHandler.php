9418e5ee249f3809ed86543571d0d10ca6f814ae
Arash <arashabedi998@gmail.com>
2021-09-25 18:01:18 +0330

    use thired party helper class

--- app/Controllers/MessageHandler.php
+++ app/Controllers/MessageHandler.php
@@ -2,18 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Helpers\ThiredPartyClass;
 use App\Modules\ExampleModule;
-use Src\InlineKeyboardMarkup;
 
 class MessageHandler{
    public function run(){
       //do somethings
-      //ExampleModule::run()->example();
-      $rows=[
-         [
-            ['text'=>'messages','switch_inline_query_current_chat'=>'msgs'],
-         ]
-         ];
-      bot()->sendMessage(['text'=>'ok','reply_markup'=>InlineKeyboardMarkup::create($rows)]);
+      ExampleModule::run()->example();
+
+      //use Thired party class
+      ThiredPartyClass::run()->sayHello();
    }
 }
