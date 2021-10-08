c0199c2b69f9008f5f3d9267a24902f543749144
Arash <arashabedi998@gmail.com>
2021-10-08 22:15:20 +0330

    edited conf file

--- config/env.php
+++ config/env.php
@@ -1,16 +1,15 @@
 <?php
-define('DOMAIN','https://60e0-185-233-150-4.ngrok.io');
+$domain='https://60e0-185-233-150-4.ngrok.io';
 
 
 
 return [
     'token'=>'632423848:AAECin8sUe_VdUqdAKnq8XlsAKSpUvqE2nU',
-    'domain'=>DOMAIN,
-    'request_handler_path'=>DOMAIN.'/requestsHandler.php',
-    'bot_main_path'=>DOMAIN.'/bootstrap/bot.php',
+    'domain'=>$domain,
+    'request_handler_path'=>$domain.'/requestsHandler.php',
+    'bot_main_path'=>$domain.'/bootstrap/bot.php',
     'DB_CONNECTION'=>'sqlite',
     'DB_NAME'=>'telebot',
     'DB_USERNAME'=>'arash',
     'DB_PASSWORD'=>'arash',
-    
 ];
