4b3064764bad7e2b290c746ddf0b5eb68e3d925a
Arash <arashabedi998@gmail.com>
2021-10-08 22:16:10 +0330

    updated stff

--- test.php
+++ test.php
@@ -7,16 +7,5 @@ use Database\Db;
 require 'vendor/autoload.php';
 
 
-// echo '<pre>';
-// var_dump(json_decode(file_get_contents("bootstrap/debug.txt"),true));
-// echo '</pre>';
-
-//echo config()['DB_CONNECTION'];
-// $db=Db::getInstance();
-// echo '<pre>';
-// var_dump($db->select('select * from users'));
-// echo '</pre>';
-
-$user=new User();
-
-var_dump($user->getUser(1));
+$data=file_get_contents("config/env.php");
+var_dump($data);
