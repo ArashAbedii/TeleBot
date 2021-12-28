c7322cef3765a134ea262b4d28c7a2e45d80b3f0
arash <arashabedi998@gmail.com>
2021-12-28 06:23:06 -0500

    updated

--- config/env.php
+++ config/env.php
@@ -4,7 +4,7 @@ $domain='DOMAIN';
 
 
 return [
-    'token'=>'',
+    'token'=>'BOT_TOKEN',
     'domain'=>$domain,
     'request_handler_path'=>$domain.'/requestsHandler.php',
     'bot_main_path'=>$domain.'/bootstrap/bot.php',
