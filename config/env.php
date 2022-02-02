4f28825bfdc09e336869fb7ce582c2347c8a20a5
arash <arashabedi998@gmail.com>
2022-02-02 20:57:12 +0330

    added admin chat_id option

--- config/env.php
+++ config/env.php
@@ -1,10 +1,11 @@
 <?php
-$domain='DOMAIN';
+$domain='DOMAIN'; //PUT YOUR BOT ROOT HERE EXAMPLE: https://example.com/mybotDirectory
 
 
 
 return [
-    'token'=>'BOT_TOKEN',
+    'token'=>'BOT_TOKEN', //PUT YOUR BOT TOKEN HERE
+    'ADMIN_CHAT_ID'=>'YOUR_CHAT_ID', //PUT YOUR CHAT_ID HERE
     'domain'=>$domain,
     'request_handler_path'=>$domain.'/requestsHandler.php',
     'bot_main_path'=>$domain.'/bootstrap/bot.php',
