73ce4d2ad2e14b616abf248e16e17fdb209b1cd4
Arash Abedi <arashabedi998@gmail.com>
2022-05-01 14:52:23 +0430

    added some new options

--- config/env.php
+++ config/env.php
@@ -8,8 +8,12 @@ return [
     'request_handler_path'=>$server_address.'/requestsHandler.php',
     'bot_main_path'=>$server_address.'/bootstrap/bot.php',
     'DB_CONNECTION'=>'mysql', //or sqlite
-    'DB_NAME'=>'telebot',
-    'DB_USERNAME'=>'root',
-    'DB_PASSWORD'=>'',
+    'DB_HOST'=>'localhost',
+    'DB_NAME'=>'telebot', //database name
+    'DB_USERNAME'=>'root', //database username
+    'DB_PASSWORD'=>'', //database password
+    'DB_CHARSET'=>'utf8',
+    'DB_COLLATION'=>'utf8_unicode_ci',
+    'APP_BASE_PATH'=>dirname(__DIR__),
 ];
 
