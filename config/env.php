571f60783a94418fbff5199edbdb7c9c7766bbda
arash Abedi <arashabedi998@gmail.com>
2022-07-21 15:59:10 +0430

    fixed

--- config/env.php
+++ config/env.php
@@ -1,9 +1,9 @@
 <?php
-$server_address='http://localhost:8000'; //PUT YOUR BOT ROOT HERE EXAMPLE: http://192.168.1.9:8000/mybotDirectory
+$server_address='SERVER ADDRES'; //PUT YOUR BOT ROOT HERE EXAMPLE: http://192.168.1.9:8000/mybotDirectory
 
 return [
-    'token'=>'5482246466:AAG3XKnY7LhP8ce2FUEtIVHIuFWkD6cEmTs', //PUT YOUR BOT TOKEN HERE
-    'ADMIN_CHAT_ID'=>'724490572', //PUT YOUR CHAT_ID HERE
+    'token'=>'BOT_TOKEN', //PUT YOUR BOT TOKEN HERE
+    'ADMIN_CHAT_ID'=>'CHAT_ID', //PUT YOUR CHAT_ID HERE
     'domain'=>$server_address,
     'request_handler_path'=>$server_address.'/requestsHandler.php',
     'bot_main_path'=>$server_address.'/bootstrap/bot.php',
